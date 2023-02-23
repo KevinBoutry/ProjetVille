27190f17c098168c7354ab2b96cda1a91718f263
Judicael45 <117268223+Judicael45@users.noreply.github.com>
2023-02-23 16:56:55 +0100

    ProjetVille

--- accueil.php
+++ accueil.php
@@ -20,16 +20,16 @@ require "./ressources/template/_header.php";
         <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
 
         </p>
-       <p>
+    <p>
 
-           <label><b>Mot de passe</b></label>
-           <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+        <label><b>Mot de passe</b></label>
+        <input type="password" placeholder="Entrer le mot de passe" name="password" required>
         </p>
         
         <input type="submit" id='submit' value='LOGIN'>
         
         <br>
-        <label><a href="/inscription">créer un compte</a></label>
+        <label><a href="/inscription.php">créer un compte</a></label>
         <?php
         if (isset($_POST['erreur'])) {
             $err = $_POST['erreur'];
